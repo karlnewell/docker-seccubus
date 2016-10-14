929363d4e4df763e10c656f17f31e636b0cc037e
Karl Newell <karlnewell@gmail.com>
2016-10-13 19:55:31 -0700

    entrypoint.sh is reentrant.  Update README.  Clean up Dockerfile

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Container build for https://www.seccubus.com/
 ```
 docker run -dit -p 80:80 -v $SOMEDIR:/var/lib/mysql karlnewell/seccubus
 ```
+
+entrypoint.sh is reentrant.  Mount /var/lib/mysql from a host directory to preserve database. 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,12 @@ set -e
 
 service apache2 start
 
-wget https://github.com/schubergphilis/Seccubus_v2/releases/download/2.26/seccubus_2.26.1_amd64.deb 
-dpkg --force-all -i seccubus_2.26.1_amd64.deb
+if [ ! -f "/seccubus_2.26.1_amd64.deb" ]; then #check whether seccubus is already installed
+    wget https://github.com/schubergphilis/Seccubus_v2/releases/download/2.26/seccubus_2.26.1_amd64.deb 
+    dpkg --force-all -i seccubus_2.26.1_amd64.deb
+
+    cp /etc/apache2/sites-available/seccubus.working /etc/apache2/sites-available/seccubus.conf
+fi
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then #check whether the DB is initialized.
     echo 'Initializing database'
@@ -21,10 +25,7 @@ EOF
 
     /usr/bin/mysql -u seccubus -pseccubus seccubus < /var/lib/seccubus/structure_v8.mysql
     /usr/bin/mysql -u seccubus -pseccubus seccubus < /var/lib/seccubus/data_v8.mysql
-
 fi
-
-mv /etc/apache2/sites-available/seccubus.working /etc/apache2/sites-available/seccubus.conf
 
 service mysql restart
 service apache2 restart
